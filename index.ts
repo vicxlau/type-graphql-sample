2e7721510a7f030c3f3c3dc8d0950eb0b296914e
Vicx Lau <vicx.wy.lau@hk.pwc.com>
2020-01-28 18:49:54 +0800

    init

---
+++ index.ts
@@ -0,0 +1,27 @@
+import "reflect-metadata";
+import { ApolloServer } from "apollo-server";
+import { buildSchema } from "type-graphql";
+
+import { SampleResolver } from "./resolver";
+
+async function bootstrap() {
+  // Build the TypeGraphQL schema
+  const schema = await buildSchema({
+    resolvers: [SampleResolver],
+  });
+
+  // Create GraphQL server
+  const server = new ApolloServer({
+    schema,
+    playground: true,
+    // you can pass the endpoint path for subscriptions
+    // otherwise it will be the same as main graphql endpoint
+    // subscriptions: "/subscriptions",
+  });
+
+  // Start the server
+  const { url } = await server.listen(4000);
+  console.log(`Server is running, GraphQL Playground available at ${url}`);
+}
+
+bootstrap();
